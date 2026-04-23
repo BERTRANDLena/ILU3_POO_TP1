ded07944eb85c8baa0a7732122c7de66daaf3887
BERTRANDLena <lena.bertrand@univ-tlse3.fr>
2026-04-23 09:30:40 +0200

    TP3 partie3

--- src/jeu/Joueur.java
+++ src/jeu/Joueur.java
@@ -1,29 +1,35 @@
 package jeu;
 
+import cartes.Bataille;
+import cartes.Borne;
 import cartes.Carte;
+import cartes.FinLimite;
+import cartes.Limite;
 
 public class Joueur {
-	
+
 	private String nom;
 	private ZoneDeJeu zoneDeJeu;
 	private MainJoueur main;
-	
+
 	public Joueur(String nom, ZoneDeJeu zoneDeJeu, MainJoueur main) {
 		this.nom = nom;
 		this.zoneDeJeu = zoneDeJeu;
 		this.main = main;
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
+
 	public ZoneDeJeu getZoneDeJeu() {
 		return zoneDeJeu;
 	}
+
 	public MainJoueur getMainJoueur() {
 		return main;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -32,18 +38,35 @@ public class Joueur {
 			return false;
 
 		Joueur autre = (Joueur) obj;
-		return nom!=null && nom.equals(autre.nom);
+		return nom != null && nom.equals(autre.nom);
 	}
 
 	@Override
 	public String toString() {
 		return nom;
 	}
-	
+
 	public void donner(Sabot sabot, MainJoueur main) {
 		Carte carte = sabot.piocher();
-		
-		
+		main.prendre(carte);
+	}
+
+	public Carte prendreCarte(Sabot sabot) {
+		Carte carte = sabot.piocher();
+		if (carte != null) {
+			main.prendre(carte);
+		}
+		return carte;
+	}
+
+	public int donnerKmParcourus() {
+		return zoneDeJeu.donnerKmParcourus();
 	}
 	
+	public void deposer(Carte carte) {
+		if (carte == null) {
+	        return;
+	    }
+		zoneDeJeu.deposer(carte);    
+	}
 }
--- src/jeu/ZoneDeJeu.java
+++ src/jeu/ZoneDeJeu.java
@@ -5,19 +5,48 @@ import java.util.List;
 
 import cartes.Bataille;
 import cartes.Borne;
+import cartes.Carte;
+import cartes.FinLimite;
 import cartes.Limite;
 
 public class ZoneDeJeu {
-	
-	private List<Limite> pileLimite;
-	private List<Bataille> pileBataille;
-	private List<Borne> bornes;
-	
-	public ZoneDeJeu(){
-		this.pileLimite = new LinkedList<Limite>();
-		this.pileBataille = new LinkedList<Bataille>();
-		this.bornes = new LinkedList<Borne>();
+
+	private List<Limite> pileLimite = new LinkedList<Limite>();
+	private List<Bataille> pileBataille = new LinkedList<Bataille>();
+	private List<Borne> pileBornes = new LinkedList<Borne>();
+
+
+	public int donnerLimitationVitesse() {
+		if (pileLimite.isEmpty() || pileLimite.get(pileLimite.size() - 1) instanceof FinLimite) {
+			return 200;
+		}
+		return 50;
+	}
+
+	public int donnerKmParcourus() {
+		int total = 0;
+		for (Borne borne : pileBornes) {
+			total += borne.getKm();
+		}
+		return total;
 	}
 	
+	public void deposer(Carte carte) {
+
+	    if (carte instanceof Borne) {
+	        pileBornes.add((Borne) carte);
+	    }
+	    else if (carte instanceof Limite || carte instanceof FinLimite) {
+	        pileLimite.add((Limite) carte);
+	    }
+	    else if (carte instanceof Bataille) {
+	        pileBataille.add((Bataille) carte);
+	    }
+	}
 	
+	public boolean peutAvancer() {
+		Bataille sommet = pileBataille.get(0);
+		return (!pileBataille.isEmpty() && sommet.equals(Cartes.FEU_VERT));
+	
+	}
 }
